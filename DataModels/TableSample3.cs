c415dccd0f474940f30d669587bc74ae3342e7d6
Ye Ko <yeko_min@nec.com.sg>
2017-07-12 16:16:37 +0800

    Test

---
+++ DataModels/TableSample3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class TableSample3
+    {
+        /*replace below codes with data model fields*/
+
+        public int Id { get; set; }
+
+        public int ContractId { get; set; }
+    }
+}

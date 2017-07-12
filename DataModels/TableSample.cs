a567afcccf740dd1182d3240ff309425525f0d05
lanliang <iorilan@hotmail.com>
2017-07-12 16:10:42 +0800

    a

---
+++ DataModels/TableSample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class TableSample
+    {
+        /*replace below codes with data model fields*/
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string EquipmentUniqueId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Location { get; set; }
+
+        public bool? IsPhysical { get; set; }
+
+        [StringLength(100)]
+        public string EquipmentType { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? CommisionDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? DecommissionDate { get; set; }
+
+        public decimal? PriceAtOwnerShip { get; set; }
+
+        [StringLength(255)]
+        public string WarrantyDescription { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? PreventiveMaintenanceDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? ExpiryDate { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; }
+
+        public int ContractId { get; set; }
+    }
+}
--- NetGenerator/BusinessLayer/ReflectObj.cs
+++ NetGenerator/BusinessLayer/ReflectObj.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+
+/// <summary>
+/// This code is generated from NetGenerator
+/// Business Obj : TableSampleObj
+/// </summary>
+public class TableSampleObj
+{
+	public Int32 Id { get; set; }
+
+	public String EquipmentUniqueId { get; set; }
+
+	public String Name { get; set; }
+
+	public String Location { get; set; }
+
+	public Boolean IsPhysical { get; set; }
+
+	public String EquipmentType { get; set; }
+
+	public DateTime CommisionDate { get; set; }
+
+	public DateTime DecommissionDate { get; set; }
+
+	public Decimal PriceAtOwnerShip { get; set; }
+
+	public String WarrantyDescription { get; set; }
+
+	public DateTime PreventiveMaintenanceDate { get; set; }
+
+	public DateTime ExpiryDate { get; set; }
+
+	public String Status { get; set; }
+
+	public Int32 ContractId { get; set; }
+
+}

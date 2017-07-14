ce79bedf73e9efa4a323e288cb709cd3356dfb12
Ye Ko <yeko_min@nec.com.sg>
2017-07-14 12:52:17 +0800

    login

---
+++ NetGenerator/UILayer/Models/Constants.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YourProject.Models
+{
+    public class UIKeys
+    {
+        public const string IsSuccess = "IsSuccess";
+        public const string IsError = "IsError";
+        public const string Message = "Message";
+    }
+}
